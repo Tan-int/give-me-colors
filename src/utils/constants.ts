2e3ea8448bcab7443341832ce87f2aa5f46cf4e9
ANDERSENCORP\a93190 <a93910@andersencorp.com>
2024-09-09 10:10:06 -0500

    chore: create enum for color code types

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,9 +1,9 @@
 export const DEFAULT_HEX_CODE = '#BF3131';
 export const DEFAULT_RGB_CODE = 'rgb(191, 49, 49)';
 
-export const COLOR_CODE_TYPE = {
-  DEFAULT: 0,
-  HEX: 1,
-  RGB: 2,
-  HSL: 3,
-};
+export enum COLOR_CODE_TYPE {
+  DEFAULT = 0,
+  HEX = 1,
+  RGB = 2,
+  HSL = 3,
+}
