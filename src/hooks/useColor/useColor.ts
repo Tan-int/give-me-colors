4e286982db8574b86f6e9d73d0896060e924d578
ANDERSENCORP\a93190 <a93910@andersencorp.com>
2024-09-09 10:11:13 -0500

    feat: create functions to get the rgb code from hex

--- src/hooks/useColor/useColor.ts
+++ src/hooks/useColor/useColor.ts
@@ -1,15 +1,67 @@
-export function useColor(colorCode: string) {
-  const color = {
-    hex: rgbToHex(colorCode),
-    rgb: hexToRgb(colorCode),
-    hsl: '',
+import { COLOR_CODE_TYPE } from '@/utils/constants';
+
+let lastValidColorCode = 'rgb(0, 191, 255)';
+let hex = '';
+let rgb = '';
+
+export const color = (colorCode: string) => {
+  const colorCodeType = getColorCodeType(colorCode);
+
+  if (colorCodeType) {
+    lastValidColorCode = colorCode;
+    hex = getHexCode(colorCode, colorCodeType);
+    rgb = getRgbCode(colorCode, colorCodeType);
+  }
+
+  console.log(hex);
+  return {
+    colorCode: lastValidColorCode,
+    rgb: rgb,
+    hex: hex,
   };
+};
 
-  return color;
-}
+const getRgbCode = (colorCode: string, colorCodeType: COLOR_CODE_TYPE) => {
+  const match = colorCode
+    .replace(/[^\d,]/g, '')
+    .split(',')
+    .filter(Boolean);
 
-export const rgbToHex = (colorCode: string) => {
-  const match = colorCode.match(/rgb\((\d+),\s*(\d+),\s*(\d+)\)/);
+  let r = 0,
+    g = 0,
+    b = 0;
+  if (colorCodeType === COLOR_CODE_TYPE.HEX) {
+    [r, g, b] = hexToRgb(colorCode);
+  }
+
+  if (colorCodeType === COLOR_CODE_TYPE.HSL) {
+    // hsl to rgb code here
+  }
+
+  if (match) {
+    [r, g, b] = match.map(hex => parseInt(hex));
+  }
+
+  return toRgbString(r, g, b);
+};
+
+const getHexCode = (colorCode: string, colorCodeType: COLOR_CODE_TYPE) => {
+  if (colorCodeType === COLOR_CODE_TYPE.RGB) {
+    return rgbToHex(colorCode);
+  }
+
+  if (colorCodeType === COLOR_CODE_TYPE.HSL) {
+    // hsl to hex here
+  }
+
+  return colorCode;
+};
+
+const rgbToHex = (colorCode: string) => {
+  const match = colorCode
+    .replace(/[^\d,]/g, '')
+    .split(',')
+    .filter(Boolean);
 
   const toHex = (colorChannel: number) => {
     const hex = colorChannel.toString(16);
