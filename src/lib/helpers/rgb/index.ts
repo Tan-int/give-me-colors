932c6d26ee71294989f21fced48f5b0121a0d852
ANDERSENCORP\a93190 <a93910@andersencorp.com>
2024-09-09 15:06:20 -0500

    fix: default missing rgb values to 0

--- src/lib/helpers/rgb/index.ts
+++ src/lib/helpers/rgb/index.ts
@@ -1,16 +1,10 @@
 import { MAX_COLOR_CHANNEL_VALUE } from '@/lib/utils/constants';
 
 export const getRgbCode = (match: string[]) => {
-  let r = 0,
-    g = 0,
-    b = 0;
-
-  if (match) {
-    [r, g, b] = match.map(hex => {
-      const value = parseInt(hex);
-      return value > MAX_COLOR_CHANNEL_VALUE ? MAX_COLOR_CHANNEL_VALUE : value;
-    });
-  }
+  const [r = 0, g = 0, b = 0] = match.map(hex => {
+    const value = parseInt(hex);
+    return value > MAX_COLOR_CHANNEL_VALUE ? MAX_COLOR_CHANNEL_VALUE : value;
+  });
 
   return [r, g, b];
 };
