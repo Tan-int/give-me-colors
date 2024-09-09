43b0fc18075ef8168079b1d435fac3b54978dd69
ANDERSENCORP\a93190 <a93910@andersencorp.com>
2024-09-09 12:33:01 -0500

    fix: set max color channel value of 255 if the input exceeds it

--- src/lib/helpers/rgb/index.ts
+++ src/lib/helpers/rgb/index.ts
@@ -1,3 +1,5 @@
+import { MAX_COLOR_CHANNEL_VALUE } from '@/lib/utils/constants';
+
 export const getRgbCode = (match: string[]) => {
   let r = 0,
     g = 0,
@@ -7,6 +9,11 @@ export const getRgbCode = (match: string[]) => {
     [r, g, b] = match.map(hex => parseInt(hex));
   }
 
+  if (r > MAX_COLOR_CHANNEL_VALUE) r = MAX_COLOR_CHANNEL_VALUE;
+
+  if (g > MAX_COLOR_CHANNEL_VALUE) g = MAX_COLOR_CHANNEL_VALUE;
+
+  if (b > MAX_COLOR_CHANNEL_VALUE) b = MAX_COLOR_CHANNEL_VALUE;
   return [r, g, b];
 };
 
--- src/lib/utils/constants.ts
+++ src/lib/utils/constants.ts
@@ -1,6 +1,8 @@
 export const DEFAULT_HEX_CODE = '#BF3131';
 export const DEFAULT_RGB_CODE = 'rgb(191, 49, 49)';
 
+export const MAX_COLOR_CHANNEL_VALUE = 255;
+
 export enum COLOR_CODE_TYPE {
   DEFAULT = 0,
   HEX = 1,
