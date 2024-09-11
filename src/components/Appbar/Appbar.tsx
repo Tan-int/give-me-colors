85a2025068cc4a7ad4d6e8787e965437141d902d
ANDERSENCORP\a93190 <a93910@andersencorp.com>
2024-09-11 15:14:53 -0500

    feat: add github icon to home app bar

--- src/components/Appbar/Appbar.tsx
+++ src/components/Appbar/Appbar.tsx
@@ -1,13 +1,15 @@
-import logo from '@/assets/logo.png';
+import { logo } from '@/assets';
 import ThemeToggle from '@components/ThemeToggle';
+import Github from '@components/GithubIcon';
 
 export default function Appbar() {
   return (
-    <div className="flex w-full justify-between px-2 py-2">
-      <div className="relative w-12">
+    <div className="flex w-full justify-between px-3 py-2">
+      <div className="h-auto w-12">
         <img src={logo} />
       </div>
-      <div className="flex w-full justify-end px-3">
+      <div className="flex w-full items-center justify-end gap-x-4 pr-3">
+        <Github />
         <ThemeToggle />
       </div>
     </div>
