de1e3526fdd95cbcc266bc03af2afce8a44bad3c
Tan Duong <tantanduong1@gmail.com>
2024-09-19 10:51:05 -0500

    refactor: open github repo in new tab

--- src/components/Appbar/Appbar.tsx
+++ src/components/Appbar/Appbar.tsx
@@ -9,7 +9,7 @@ export default function Appbar() {
         <img src={logo} />
       </div>
       <div className="flex w-full items-center justify-end gap-x-4 pr-3">
-        <a href="https://github.com/Tan-int/give-me-colors">
+        <a href="https://github.com/Tan-int/give-me-colors" target="_blank">
           <Github />
         </a>
         <ThemeToggle />
