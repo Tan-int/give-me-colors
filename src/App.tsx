786ea3a71a1935ec78c52bb9d96fa97b3bf5be74
Tan Duong <tantanduong1@gmail.com>
2024-09-19 12:49:41 -0500

    style: layout styling for medium and large screens

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { colorPicker } from '@/lib/services/colorPicker';
+import ColorInfoContainer from '@components/ColorInfoContainer';
 import { INITIAL_COLOR_CODE } from '@/lib/utils/constants';
 import Appbar from '@components/Appbar';
 import Input from '@components/Input';
@@ -15,26 +16,32 @@ function App() {
   };
 
   return (
-    <div className="flex h-screen flex-col">
+    <div className="flex h-screen flex-col pb-4">
       <Appbar />
       <div className="flex h-full w-full flex-col items-center px-12 pb-12">
-        <div className="mb-6 mt-4 flex w-full flex-col items-center">
+        <div className="mb-6 flex w-full flex-col items-center">
           <h1 className="text-2xl font-semibold">Color Converter</h1>
           <p className="text-sm opacity-60">
             Convert between HEX, RGB, and HSL color formats
           </p>
         </div>
-        <div className="flex h-full w-full flex-col space-y-4 p-6">
-          <Input
-            value={input}
-            onChange={handleInputChange}
-            className="w-full text-base"
-          />
-          <ColorInfo colorModel="HEX" colorCode={color.hex} />
-          <ColorInfo colorModel="HSL" colorCode={color.hsl} />
-          <ColorInfo colorModel="RGB" colorCode={color.rgb} />
+        <div className="flex h-full w-full flex-col gap-y-8 lg:flex-row lg:gap-x-8">
+          <div className="flex w-full flex-col gap-y-8">
+            <Input
+              value={input}
+              onChange={handleInputChange}
+              className="text-base"
+            />
+            <ColorInfoContainer>
+              <ColorInfo colorModel="HEX" colorCode={color.hex} />
+              <hr />
+              <ColorInfo colorModel="HSL" colorCode={color.hsl} />
+              <hr />
+              <ColorInfo colorModel="RGB" colorCode={color.rgb} />
+            </ColorInfoContainer>
+          </div>
           <div
-            className="h-full w-full"
+            className="size-full min-h-[200px]"
             style={{ background: color.hex }}
           ></div>
         </div>
