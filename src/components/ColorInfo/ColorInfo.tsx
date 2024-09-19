ae39f7e3019d368c52699e39e93c993b5b558432
Tan Duong <tantanduong1@gmail.com>
2024-09-19 12:34:34 -0500

    style: layout of color info component

--- src/components/ColorInfo/ColorInfo.tsx
+++ src/components/ColorInfo/ColorInfo.tsx
@@ -1,13 +1,20 @@
+import { cn } from '@/lib/utils/cn';
+
 type ColorInfoProps = {
+  className?: string | undefined;
   colorModel: string;
   colorCode: string;
 };
 
-export default function ColorInfo({ colorModel, colorCode }: ColorInfoProps) {
+export default function ColorInfo({
+  className,
+  colorModel,
+  colorCode,
+}: ColorInfoProps) {
   return (
-    <div className="flex flex-col gap-y-1">
+    <div className={cn('flex flex-col gap-y-2', className)}>
       <h6 className="text-sm">{colorModel}</h6>
-      <div className="flex flex-col gap-y-1 border px-3 py-2">
+      <div className="flex flex-col gap-y-1">
         <p className="text-sm opacity-90">{colorCode}</p>
       </div>
     </div>
