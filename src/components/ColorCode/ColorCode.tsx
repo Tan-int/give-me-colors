ef5c6d28b24f73dd5d6adc219ac665452f5fc36c
Tan Duong <tantanduong1@gmail.com>
2024-09-20 12:06:08 -0500

    feat: implement copy to clipboard function

--- src/components/ColorCode/ColorCode.tsx
+++ src/components/ColorCode/ColorCode.tsx
@@ -5,8 +5,15 @@ type ColorCodeProps = {
 };
 
 export default function ColorCode({ colorCode }: ColorCodeProps) {
+  const copyToClipboard = () => {
+    navigator.clipboard.writeText(colorCode);
+  };
+
   return (
-    <div className="flex w-full flex-row items-center justify-between">
+    <div
+      className="flex w-full flex-row items-center justify-between"
+      onClick={copyToClipboard}
+    >
       <p className="text-sm opacity-90">{colorCode}</p>
       <Copy className="h-4 w-4" />
     </div>
