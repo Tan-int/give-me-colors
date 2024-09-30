d996c47c2fdef8fc9be847c0f6a0f4cf983dc93d
Tan Duong <tantanduong1@gmail.com>
2024-09-30 13:35:12 -0500

    fix: color swatch lists would generate more than five colors when changing colors

--- src/components/ColorSwatchList/ColorSwatchList.tsx
+++ src/components/ColorSwatchList/ColorSwatchList.tsx
@@ -7,8 +7,8 @@ type ColorSwatchListProps = {
 export default function ColorSwatchList({ colorCodes }: ColorSwatchListProps) {
   return (
     <div className="flex flex-row gap-x-6 md:w-full md:gap-x-1 lg:flex-col-reverse">
-      {colorCodes.map(color => (
-        <ColorSwatch key={color} hex={color} />
+      {colorCodes.map((color, index) => (
+        <ColorSwatch key={index} hex={color} />
       ))}
     </div>
   );
