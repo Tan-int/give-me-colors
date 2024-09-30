bd02e07fe778fbe18753bcb4a65facc0b37cc4f4
Tan Duong <tantanduong1@gmail.com>
2024-09-30 14:13:20 -0500

    feat: copy color code of color swatches

--- src/components/ColorSwatch/ColorSwatch.tsx
+++ src/components/ColorSwatch/ColorSwatch.tsx
@@ -1,14 +1,29 @@
+import useCopyToClipboard from '@/hooks/useCopyToClipboard';
+import { ClipboardCheck } from 'lucide-react';
+
 type ColorSwatchProps = {
   hex: string;
 };
 
 export default function ColorSwatch({ hex }: ColorSwatchProps) {
   // const hexToUpper = hex.toUpperCase();
+  const { isCopied, copyToClipboard } = useCopyToClipboard();
+
+  const handleClick = () => {
+    copyToClipboard(hex);
+  };
 
   return (
     <div
-      className="h-7 w-7 rounded-full md:h-16 md:w-full md:rounded-none lg:w-full"
+      className="flex size-full h-7 w-7 items-center justify-center rounded-full md:h-16 md:w-full md:rounded-none lg:w-full"
       style={{ backgroundColor: hex }}
-    ></div>
+      onClick={handleClick}
+    >
+      {isCopied && (
+        <div className="input-stepper rounded-full p-1">
+          <ClipboardCheck className="h-4 w-4" />
+        </div>
+      )}
+    </div>
   );
 }
