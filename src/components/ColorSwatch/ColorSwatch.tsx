b355fd98ec2f42e75c21aa7d57e6e21b481706ac
Tan Duong <tantanduong1@gmail.com>
2024-10-01 16:41:50 -0500

    style: maintain light text color regardless of color theme

--- src/components/ColorSwatch/ColorSwatch.tsx
+++ src/components/ColorSwatch/ColorSwatch.tsx
@@ -21,13 +21,13 @@ export default function ColorSwatch({ hex }: ColorSwatchProps) {
     >
       {isCopied && (
         <div className="rounded-full bg-black/30 p-1 md:hidden">
-          <ClipboardCheck className="h-4 w-4" />
+          <ClipboardCheck className="h-4 w-4" color="#F6F8F9" />
         </div>
       )}
 
       <div className="invisible hidden min-w-[70px] rounded-tl-md bg-black/30 p-1 group-hover:visible md:absolute md:bottom-0 md:right-0 md:block">
         <div className="flex w-full justify-center">
-          <p className="text-xs font-semibold">
+          <p className="text-xs font-semibold text-[#F6F8F9]">
             {isCopied ? 'Copied' : hexToUpper}
           </p>
         </div>
