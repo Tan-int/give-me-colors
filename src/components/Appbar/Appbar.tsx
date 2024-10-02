56036e8c334b42cde6bcb00d4687bb37e7c6f740
Tan Duong <tantanduong1@gmail.com>
2024-10-02 11:13:27 -0500

    chore: add logo with smaller box size

--- src/components/Appbar/Appbar.tsx
+++ src/components/Appbar/Appbar.tsx
@@ -4,10 +4,10 @@ import { ThemeToggle, Github } from '@/components';
 export default function Appbar() {
   return (
     <div className="flex w-full justify-between">
-      <div className="h-auto w-12">
+      <div className="h-auto w-8">
         <img src={logo} />
       </div>
-      <div className="flex w-full items-center justify-end gap-x-4 pr-3">
+      <div className="flex w-full items-center justify-end gap-x-4">
         <a href="https://github.com/Tan-int/give-me-colors" target="_blank">
           <Github />
         </a>
