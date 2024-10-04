887bbef9e18b67cd7c7f226d404150fdb9bde36b
Tan Duong <tantanduong1@gmail.com>
2024-10-04 08:28:13 -0500

    feat: install analytics from vercel

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,13 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 import './index.css';
 import { ThemeProvider } from './contexts/themeContext.js';
+import { Analytics } from '@vercel/analytics/react';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ThemeProvider>
       <App />
+      <Analytics />
     </ThemeProvider>
   </StrictMode>
 );
